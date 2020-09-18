99a9b2d54d8ea3e31562f604c889825cc4f88cf6
shivkhaira <shivsinghkhaira@gmail.com>
2020-09-18 23:18:54 +0530

    Try2

--- github/settings.py
+++ github/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -82,10 +82,6 @@ DATABASES = {
     }
 }
 
-
-db_config = dj_database_url.config()
-if db_config:
-    DATABASES['default'] =  db_config
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
