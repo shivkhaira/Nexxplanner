af0c8297c49d332cae2347bc64b875356018f3ea
shivkhaira <shivsinghkhaira@gmail.com>
2020-09-20 01:39:33 +0530

    Crontab

--- view/myscript.py
+++ view/myscript.py
@@ -1,21 +1,21 @@
-from django.core.mail import send_mail
-import os
-from django.conf import settings
-
-
-
-if __name__ == '__main__' and __package__ is None:
-    os.sys.path.append(
-        os.path.dirname(
-            os.path.dirname(
-                os.path.abspath(__file__))))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "github.github.settings")
 
 
 
 def handle():
+    from django.core.mail import send_mail
+    import os
+    from django.conf import settings
+
+    if __name__ == '__main__' and __package__ is None:
+        os.sys.path.append(
+            os.path.dirname(
+                os.path.dirname(
+                    os.path.abspath(__file__))))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "github.github.settings")
+
     subject = 'welcome to GFG world'
     message = 'Hi , thank you for registering in geeksforgeeks.'
     email_from = "shivsinghkhaira@gmail.com"
     recipient_list = ['shivsinghkhaira@gmail.com']
     send_mail(subject, message, email_from, recipient_list)
+
