5601a62b135008d67c6dff158fa44490b75b3085
shivkhaira <shivsinghkhaira@gmail.com>
2020-09-19 22:16:08 +0530

    C

--- view/cron.py
+++ view/cron.py
@@ -1,4 +1,22 @@
-from django.core.mail import send_mail
+from celery import Celery
+from celery.schedules import crontab
 
-def cron_job():
-    send_mail("hello paul", "hello", "shivsinghkhaira@gmail.com", ['shivsinghkhaira@gmail.com'])
+app = Celery('github')
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    sender.add_periodic_task(
+        crontab(hour=7, minute=30, day_of_week=1),
+        test.s('Happy Mondays!'),
+    )
+
+@app.task
+def test(arg):
+    print(arg)
