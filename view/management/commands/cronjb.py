48015818a343977e967c90f61875afc4ec48fac4
shivkhaira <shivsinghkhaira@gmail.com>
2020-09-19 19:38:23 +0530

    CronDB

--- view/management/commands/cronjb.py
+++ view/management/commands/cronjb.py
@@ -1,7 +1,11 @@
 from django.core.management.base import BaseCommand
-
+from django.core.mail import send_mail
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        a=1
+        subject = 'welcome to GFG world'
+        message = 'Hi , thank you for registering in geeksforgeeks.'
+        email_from = "shivsinghkhaira@gmail.come"
+        recipient_list = ['shivsinghkhaira@gmail.com']
+        send_mail(subject, message, email_from, recipient_list)
