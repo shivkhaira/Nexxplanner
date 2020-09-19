26c3c99f65e111cee6c8dba391ef8edfcbe97dfa
shivkhaira <shivsinghkhaira@gmail.com>
2020-09-19 12:05:47 +0530

    POST

--- github/settings.py
+++ github/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
+
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
