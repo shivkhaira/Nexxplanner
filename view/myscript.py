6be2ae61a72ee05642836a791923885f1354c78a
shivkhaira <shivsinghkhaira@gmail.com>
2020-09-20 01:33:37 +0530

    Crontab

--- view/myscript.py
+++ view/myscript.py
@@ -1,4 +1,15 @@
 from django.core.mail import send_mail
+import os
+from django.conf import settings
+
+
+
+if __name__ == '__main__' and __package__ is None:
+    os.sys.path.append(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.abspath(__file__))))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "github.github.settings")
 
 
 
