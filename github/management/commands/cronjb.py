4369ff615013fa8358a6f1d2a5f63225b38d8a9b
shivkhaira <shivsinghkhaira@gmail.com>
2020-09-19 18:18:30 +0530

    Find

---
+++ github/management/commands/cronjb.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.core.mail import send_mail
+
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        subject = 'welcome to GFG world'
+        message = 'Hi , thank you for registering in geeksforgeeks.'
+        email_from = "shivsinghkhaira@gmail.come"
+        recipient_list = ['shivsinghkhaira@gmail.com']
+        send_mail(subject, message, email_from, recipient_list)
