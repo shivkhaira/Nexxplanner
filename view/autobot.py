e429e4af48fa11482f7b48efc622c0c6c3274f9b
shivkhaira <shivsinghkhaira@gmail.com>
2020-09-20 14:47:12 +0530

    All Three

--- view/autobot.py
+++ view/autobot.py
@@ -4,6 +4,9 @@ import os
 import PIL
 import numpy as np
 import shutil
+import facebook
+import tweepy
+
 def crop(x, y, data, w, h):
     x = int(x)
     y = int(y)
@@ -86,3 +89,23 @@ class Int:
         bot.upload_photo(pici, caption=caption)
         bot.logout()
         shutil.rmtree(dd)
+
+    def face(pic,caption):
+        page_access_token = "EAAKuZCOV5iuEBABG7XeqTy7Fyp7aDrtwfdrUkRF8mZCZCxFbZBeJtJZAVK6EPfih19ZCm4UxtkZBZBscq07jxaXUZCZCfIJr5Q4UFCniBURfrOKfUoN0Ab54ZBxEHNQdboKqZAnN3IZCIf5FwXTvZBGGkkk16at3pKPt1G1RCGXNK9XdYfUgZDZD"
+        graph = facebook.GraphAPI(page_access_token)
+        graph.put_photo(open(pic,'rb'),message=caption)
+
+    def twit(pic,caption):
+        consumer_key = "iYZI295ISSEV3JbDwnNGnWUDX"
+        consumer_secret = "Xn9tiYRtq3i5PbzWc1d1oZYX6sTcGGgtCDH4hnqAxZPiKm5QV7"
+        access_token = "1019098989956775936-tly7ewKTrnG0nQuIIhgLjr7lyommn1"
+        access_token_secret = "6m6IpcwyVzx7TUin7tVMO9yQP4RkaCw5deactmPZ2Wktj"
+
+        # authentication of consumer key and secret
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+        # authentication of access token and secret
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+
+        api.update_with_media(filename=pic, status=caption)
--- view/management/commands/pet.py
+++ view/management/commands/pet.py
@@ -1,15 +1,18 @@
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
 import requests
+import multiprocessing
 
-
-from view.models import Insta
-
+from view.models import Insta,Iupload
+from view.autobot import Int
+from view.instabot import Bot
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         p = Insta.objects.get(id=1)
-        subject = p.username
+        multiprocessing.Process(target=Int.up, args=(p.username, p.password, 'media/' + img, data.caption)).start()
+        print(p.file)
+        subject = p.caption
         message = 'Hi , thank you for registering in geeksforgeeks.'
         email_from = "shivsinghkhaira@gmail.com"
         recipient_list = ['shivsinghkhaira@gmail.com']
