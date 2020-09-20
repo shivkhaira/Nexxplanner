7d3f44ed955e5d04446de746ee73ed74dd59098d
shivkhaira <shivsinghkhaira@gmail.com>
2020-09-20 13:18:18 +0530

    Credit

--- view/management/commands/pet.py
+++ view/management/commands/pet.py
@@ -1,18 +1,15 @@
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
 import requests
-from ....view.instabot import Bot
 
-from ....view.models import Pro,Insta,Iupload
+
+from view.models import Insta
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-
-        name = Insta.objects.all()[0]
-        print(name['username'])
-
-        subject = name
+        p = Insta.objects.get(id=1)
+        subject = p.username
         message = 'Hi , thank you for registering in geeksforgeeks.'
         email_from = "shivsinghkhaira@gmail.com"
         recipient_list = ['shivsinghkhaira@gmail.com']
