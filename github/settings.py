8894d37143286403a23a95733efb9a008c4bd910
shivkhaira <shivsinghkhaira@gmail.com>
2020-12-10 20:24:37 +0530

    User Panel ICO

--- github/settings.py
+++ github/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'github.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
